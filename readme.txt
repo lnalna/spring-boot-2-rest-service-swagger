6674f8eefc4d9f5f6f0376470cd8f990d82a3c66
nik <lnalna@yandex.ru>
2018-09-29 16:16:40 +0300

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,3 @@
+http://www.springboottutorial.com/spring-boot-swagger-documentation-for-rest-services
+
+http://localhost:8080/h2-console     //  jdbc:h2:mem:testdb
---
+++ src/main/java/com/in28minutes/springboot/rest/example/student/StudentNotFoundException.java
@@ -0,0 +1,8 @@
+package com.in28minutes.springboot.rest.example.student;
+
+public class StudentNotFoundException extends RuntimeException {
+
+    public StudentNotFoundException(String exception) {
+        super(exception);
+    }
+}
